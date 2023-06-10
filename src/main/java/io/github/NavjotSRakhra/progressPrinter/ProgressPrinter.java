6b2597b34fa5103efdf1cb7fa5dc2cba9625064d
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2023-06-10 18:04:44 +0530

    Made project modular, deployed it to maven central

--- src/main/java/io/github/NavjotSRakhra/progressPrinter/ProgressPrinter.java
+++ src/main/java/io/github/NavjotSRakhra/progressPrinter/ProgressPrinter.java
@@ -5,6 +5,14 @@ public class ProgressPrinter {
     private final char completeCharacter, incompleteCharacter;
     private int lastComplete;
 
+    /**
+     * Creates an instance of ProgressPrinter to display progress-bar for console.
+     *
+     * @param progressBarSize     size of the progress-bar in characters
+     * @param completeCharacter   the character that replaces {@link ProgressPrinter#incompleteCharacter} when the progress
+     *                            percent increases
+     * @param incompleteCharacter the character that is displayed representing the progress to be covered
+     */
     public ProgressPrinter(int progressBarSize, char completeCharacter, char incompleteCharacter) {
         this.progressBarSize = progressBarSize;
         this.completeCharacter = completeCharacter;
@@ -12,14 +20,30 @@ public class ProgressPrinter {
         this.lastComplete = -1;
     }
 
+    /**
+     * Creates an instance of ProgressPrinter to display progress-bar for console, with progress complete character '='
+     * and progress incompleteCharacter '-'.
+     *
+     * @param progressBarSize size of the progress-bar in characters
+     */
     public ProgressPrinter(int progressBarSize) {
         this(progressBarSize, '=', '-');
     }
 
+    /**
+     * Creates an instance of ProgressPrinter to display progress-bar for console, with progress size 100,
+     * progress complete character '=' and progress incompleteCharacter '-'.
+     */
     public ProgressPrinter() {
         this(100);
     }
 
+    /**
+     * Updates the progress on the progress bar. Once 1 is passed into the method, the progress bar is exited and
+     * cannot be updated again.
+     *
+     * @param completeFraction the fraction of progress complete in double
+     */
     public void update(double completeFraction) {
         if (completeFraction < 0 || completeFraction > 1)
             throw new IllegalArgumentException(completeFraction + "argument not allowed. Percentage must be between 0 and 100");
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+/**
+ * Module used to print and update progress of a process into the console.
+ */
+module io.github.NavjotSRakhra.progressPrinter {
+    exports io.github.NavjotSRakhra.progressPrinter;
+}
